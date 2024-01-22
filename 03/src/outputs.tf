3f25e067273119cb4f26dea67e7b7004b3377f29
dimka <ddd@ddd>
2024-01-22 14:57:04 +0300

    fix

--- 03/src/outputs.tf
+++ 03/src/outputs.tf
@@ -1,7 +1,7 @@
 
 output "vm_for_each_and_count_test" {
   value = [
-    for i in merge(yandex_compute_instance.db, yandex_compute_instance.db)  : {
+    for i in merge(yandex_compute_instance.db, yandex_compute_instance.web)  : {
       name = i.name
     }
   ]
