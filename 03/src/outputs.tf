39dfecd493ff48f699348ea68a1af0ff8068de13
dimka <ddd@ddd>
2024-01-22 15:53:10 +0300

    fix

--- 03/src/outputs.tf
+++ 03/src/outputs.tf
@@ -1,16 +1,9 @@
 output "vm_for_each_and_count_test" {
-  value = merge(
-    { for vm_name, vm_instance in yandex_compute_instance.db : vm_name => {
-        name = vm_instance.name
-        id   = vm_instance.id
-        fqdn = yandex_compute_instance.db[vm_name].network_interface.0.ip_address
-      }
-    },
-    { for vm_name, vm_instance in yandex_compute_instance.web : vm_name => {
-        name = vm_instance.name
-        id   = vm_instance.id
-        fqdn = yandex_compute_instance.web[vm_name].network_interface.0.ip_address
-      }
+  value = [
+    for i in yandex_compute_instance.web : {
+      name = i.name
+      id   = i.id
+      fqdn = i.fqdn
     }
-  )
+  ]
 }
