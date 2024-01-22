43f88276387138493394ed96e1e11c985ef8aa91
dimka <ddd@ddd>
2024-01-22 14:55:20 +0300

    fix

--- 03/src/outputs.tf
+++ 03/src/outputs.tf
@@ -1,12 +1,3 @@
-output "vm_for_each_and_count" {
-  value = [
-    for vm_name, vm_instance in yandex_compute_instance.db : {
-      name = vm_instance.name
-      id   = vm_instance.id
-      fqdn = yandex_compute_instance.db[vm_name].network_interface.0.ip_address
-    }
-  ]
-}
 
 output "vm_for_each_and_count_test" {
   value = [
