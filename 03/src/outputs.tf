53dec779e9352122ddee7205c88e4f5ab16ab4a2
dimka <ddd@ddd>
2024-01-22 15:40:00 +0300

    fix

--- 03/src/outputs.tf
+++ 03/src/outputs.tf
@@ -18,8 +18,8 @@ output "vm_for_each_and_count_test" {
 output "just_test" {
   value = [
     for i, a in var.instance_configurations_1 : {
-      name = i.name
-      ami = i.ami_id
+      name = a.name
+      ami = a.ami_id
     }
   ]
 }
