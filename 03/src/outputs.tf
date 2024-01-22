da0cbcf4a804bf1d1a1ad1dd5baec3fb02acfe28
dimka <ddd@ddd>
2024-01-22 15:47:26 +0300

    fix

--- 03/src/outputs.tf
+++ 03/src/outputs.tf
@@ -1,15 +1,15 @@
 output "vm_for_each_and_count_test" {
   value = merge(
-    { for map, list in yandex_compute_instance.db : map => {
-        name = list.name
-        id   = list.id
-        fqdn = list.fqdn
+    { for macro, mini in yandex_compute_instance.db : macro => {
+        name = mini.name
+        id   = mini.id
+        fqdn = mini.fqdn
       }
     },
-    { for map, list in yandex_compute_instance.web : map => {
-        name = list.name
-        id   = list.id
-        fqdn = list.fqdn
+    { for macro, mini in yandex_compute_instance.web : macro => {
+        name = mini.name
+        id   = mini.id
+        fqdn = mini.fqdn
       }
     }
   )
