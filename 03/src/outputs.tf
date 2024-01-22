a122a091e16affbc6a123ae5a76c444b0c15fe9c
dimka <ddd@ddd>
2024-01-22 15:24:55 +0300

    fix

--- 03/src/outputs.tf
+++ 03/src/outputs.tf
@@ -6,7 +6,7 @@ output "vm_for_each_and_count_test" {
         fqdn = list.fqdn
       }
     },
-    { for map, list in yandex_compute_instance.web : vm_name => {
+    { for map, list in yandex_compute_instance.web : map => {
         name = list.name
         id   = list.id
         fqdn = list.fqdn
