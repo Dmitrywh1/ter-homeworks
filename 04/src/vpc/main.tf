e44fb8d97c57fd3dd3e6e05d940f5d84972e462f
dimka <ddd@ddd>
2024-01-27 15:59:21 +0300

    test

--- 04/src/vpc/main.tf
+++ 04/src/vpc/main.tf
@@ -19,10 +19,10 @@ resource "yandex_vpc_network" "develop" {
 }
 
 resource "yandex_vpc_subnet" "develop" {
-  name           = var.env_name == null ? "${var.subnet_zone}" : "${var.env_name}-${var.subnet_zone}"
-  zone           = var.subnet_zone
+  name           = var.env_name == null ? "${var.vpc_zone}" : "${var.env_name}-${var.vpc_zone}"
+  zone           = var.vpc_zone
   network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = [var.default_cidr]
+  v4_cidr_blocks = [var.vpc_cidr]
 
    labels = {
     for k, v in local.labels : k => v
