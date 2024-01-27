08c0193b8d001f71d3b9b2e0f12118518293f77e
dimka <ddd@ddd>
2024-01-27 16:22:44 +0300

    test

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -1,4 +1,4 @@
-module "test_vpc" {
+module "vpc" {
   source       = "./vpc"
   env_name     = "develop"
   vpc_zone     = "ru-central1-a"
@@ -6,73 +6,59 @@ module "test_vpc" {
   vpc_name     = "Jack Black"
 }
 
+module "marketing" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=1.0.0"
+  env_name       = var.vm.analytics.env_name
+  network_id     = module.vpc.vpc_subnet.name
+  subnet_zones   = module.vpc.vpc_subnet.zone
+  subnet_ids     = module.vpc.vpc_subnet.id
+  instance_name  = var.vm.analytics.instance_name
+  instance_count = var.vm.analytics.instance_count
+  image_family   = var.vm.analytics.image_family
+  public_ip      = var.vm.analytics.public_ip
 
-##создаем облачную сеть
-#resource "yandex_vpc_network" "develop" {
-#  name = "develop"
-#}
-#
-##создаем подсеть
-#resource "yandex_vpc_subnet" "develop" {
-#  name           = "develop-ru-central1-a"
-#  zone           = "ru-central1-a"
-#  network_id     = yandex_vpc_network.develop.id
-#  v4_cidr_blocks = ["10.0.1.0/24"]
-#}
-#
-#module "marketing" {
-#  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=1.0.0"
-#  env_name       = var.vm.analytics.env_name
-#  network_id     = yandex_vpc_network.develop.id
-#  subnet_zones   = [var.vm.analytics.subnet_zones]
-#  subnet_ids     = [yandex_vpc_subnet.develop.id]
-#  instance_name  = var.vm.analytics.instance_name
-#  instance_count = var.vm.analytics.instance_count
-#  image_family   = var.vm.analytics.image_family
-#  public_ip      = var.vm.analytics.public_ip
-#
-#   labels = {
-#    project = "marketing"
-#  }
-#
-#  metadata = {
-#    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
-#    serial-port-enable = 1
-#  }
-#
-#}
-#
-#module "analytics" {
-#  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=1.0.0"
-#  env_name       = var.vm.marketing.env_name
-#  network_id     = yandex_vpc_network.develop.id
-#  subnet_zones   = [var.vm.marketing.subnet_zones]
-#  subnet_ids     = [yandex_vpc_subnet.develop.id]
-#  instance_name  = var.vm.marketing.instance_name
-#  instance_count = var.vm.marketing.instance_count
-#  image_family   = var.vm.marketing.image_family
-#  public_ip      = var.vm.marketing.public_ip
-#
-#   labels = {
-#    project = "analytics"
-#  }
-#
-#  metadata = {
-#    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
-#    serial-port-enable = 1
-#  }
-#
-#}
-#
-#data template_file "cloudinit" {
-#  template = file("${path.module}/cloud-init.yml")
-#
-#  vars = {
-#    username           = var.userdata.username
-#    ssh_public_key     = file(var.userdata.ssh_public_key)
-#    packages           = var.packages
-#  }
-#}
-#
-#
-#
+   labels = {
+    project = "marketing"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
+module "analytics" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=1.0.0"
+  env_name       = var.vm.marketing.env_name
+  network_id     = module.vpc.vpc_subnet.name
+  subnet_zones   = module.vpc.vpc_subnet.zone
+  subnet_ids     = module.vpc.vpc_subnet.id
+  instance_name  = var.vm.marketing.instance_name
+  instance_count = var.vm.marketing.instance_count
+  image_family   = var.vm.marketing.image_family
+  public_ip      = var.vm.marketing.public_ip
+
+   labels = {
+    project = "analytics"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
+data template_file "cloudinit" {
+  template = file("${path.module}/cloud-init.yml")
+
+  vars = {
+    username           = var.userdata.username
+    ssh_public_key     = file(var.userdata.ssh_public_key)
+    packages           = var.packages
+  }
+}
+
+
+
--- 04/src/vpc/outputs.tf
+++ 04/src/vpc/outputs.tf
@@ -1,9 +1,10 @@
-output "VPC_Subnet" {
-  value = [
-    yandex_vpc_subnet.develop.name,
-    yandex_vpc_subnet.develop.zone,
-    yandex_vpc_subnet.develop.network_id,
-    yandex_vpc_subnet.develop.v4_cidr_blocks,
-  ]
+output "vpc_subnet" {
+  value = {
+    name       = yandex_vpc_subnet.develop.name,
+    zone       = yandex_vpc_subnet.develop.zone,
+    network_id = yandex_vpc_subnet.develop.network_id,
+    cidr       = yandex_vpc_subnet.develop.v4_cidr_blocks
+    id         = yandex_vpc_subnet.develop.id
+  }
 }
 
