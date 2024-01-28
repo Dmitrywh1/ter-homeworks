6524d56d144512dc593ef80d727063c3a40e9585
dimka <ddd@ddd>
2024-01-28 20:35:28 +0300

    test

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -1,7 +1,8 @@
 module "vpc_prod" {
   source       = "./vpc"
   env_name     = "production"
-  subnets = [
+  vpc_name     = "production"
+  subnets      = [
     { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
     { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
     { zone = "ru-central1-c", cidr = "10.0.3.0/24" },
