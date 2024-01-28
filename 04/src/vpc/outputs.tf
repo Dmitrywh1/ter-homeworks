795903f54d7b3950674b8e91b67e21599c7e401f
dimka <ddd@ddd>
2024-01-28 20:46:28 +0300

    fix

--- 04/src/vpc/outputs.tf
+++ 04/src/vpc/outputs.tf
@@ -11,10 +11,10 @@
 
 output "vpc_subnet" {
   value = {
-    name       = yandex_vpc_subnet.subnet[count.index].name,
-    zone       = yandex_vpc_subnet.subnet[count.index].zone,
-    network_id = yandex_vpc_subnet.subnet[count.index].network_id,
-    cidr       = yandex_vpc_subnet.subnet[count.index].v4_cidr_blocks
-    id         = yandex_vpc_subnet.subnet[count.index].id
+    name       = yandex_vpc_subnet.subnet[*].name,
+    zone       = yandex_vpc_subnet.subnet[*].zone,
+    network_id = yandex_vpc_subnet.subnet[*].network_id,
+    cidr       = yandex_vpc_subnet.subnet[*].v4_cidr_blocks
+    id         = yandex_vpc_subnet.subnet[*].id
   }
 }
