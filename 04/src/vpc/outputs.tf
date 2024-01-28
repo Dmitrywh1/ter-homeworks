a57962722f85784daafeee234bb6116236e4a515
dimka <ddd@ddd>
2024-01-28 20:42:10 +0300

    test

--- 04/src/vpc/outputs.tf
+++ 04/src/vpc/outputs.tf
@@ -8,3 +8,13 @@
 #  }
 #}
 #
+
+output "vpc_subnet" {
+  value = {
+    name       = yandex_vpc_subnet.subnet.name,
+    zone       = yandex_vpc_subnet.subnet.zone,
+    network_id = yandex_vpc_subnet.subnet.network_id,
+    cidr       = yandex_vpc_subnet.subnet.v4_cidr_blocks
+    id         = yandex_vpc_subnet.subnet.id
+  }
+}
